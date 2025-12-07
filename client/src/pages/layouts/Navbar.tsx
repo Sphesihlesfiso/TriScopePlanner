410fb63868c10ad27c6f7bcffd3af8441b973589
Sphesihle Mabaso <mabasosphesihle25@gmail.com>
2025-12-07 16:51:57 +0200

    Nav Bar complete

--- client/src/pages/layouts/Navbar.tsx
+++ client/src/pages/layouts/Navbar.tsx
@@ -1,8 +1,31 @@
+import { Target ,PlusIcon,LogOut} from "lucide-react"
+import { Button } from "@/components/ui/button"
 import { ModeToggle } from "@/components/mode-toggle"
-export const NavBar = () =>{
-    return(
-        <nav>
-            <ModeToggle/>
-        </nav>
-    )
+export const NavBar = () => {
+  return (
+    <nav className="sticky p-3.5" >
+        <div className="flex align-middle justify-between">
+            <ul className="flex  gap-4 items-center">
+                <li>
+                    <Button><Target/></Button>
+                </li>
+
+                <li>
+                    <h1>Tri-Scope Planner</h1>
+                </li>
+            </ul>
+            <ul className="flex  gap-4 items-center">
+                <li>
+                    <input type="text" placeholder="Search your task" ></input>
+                </li>
+                <li>
+                   <Button> <PlusIcon/></Button>
+                </li>
+                <li>
+                    <ModeToggle/>
+                </li>
+            </ul>
+        </div>
+    </nav>
+  )
 }
