ba5fd478a0bdb56b60ee3ce4dae5c7a1c8d8744a
Sphesihle Mabaso <mabasosphesihle25@gmail.com>
2025-12-18 08:37:11 +0200

    Rgister user hash and salting finish

--- server/src/api/controllers/homeControler.ts
+++ server/src/api/controllers/homeControler.ts
@@ -1,6 +1,9 @@
 import { Request, Response } from "express";
 import { dataBase } from "../../config/db";
 import { NextFunction } from "express";
+import bcrypt from "bcrypt";
+import { hash } from "crypto";
+const saltRounds = 10;
 
 export const addUserTask = async (req: Request, res: Response) => {
   const { tittle, description, scope, start_time, end_time, date } = req.body;
@@ -62,27 +65,57 @@ export function deleteTaskById(tableName: string) {
 }
 
 export const registerUser = async (req: Request, res: Response) => {
-  const { user_email, user_name, hashed_user_password } = req.body;
+  const { user_email, user_name, plain_user_password } = req.body;
   console.log(req.body);
   try {
     const result = await dataBase.query(
       "SELECT * FROM users WHERE user_email=$1",
       [user_email]
     );
+
     if (result.rows.length == 0) {
       try {
-        const results = await dataBase.query(
-          "INSERT INTO users (user_email, user_name, hashed_user_password)VALUES($1,$2,$3)",
-          [user_email, user_name, hashed_user_password]
+        bcrypt.hash(
+          plain_user_password,
+          saltRounds,
+          async function (err, hash) {
+            // Store hash in your password DB.
+            if (!err) {
+              const results = await dataBase.query(
+                "INSERT INTO users (user_email, user_name, hashed_user_password) VALUES($1,$2,$3)",
+                [user_email, user_name, hash]
+              );
+              console.log(hash);
+              res.send(results);
+            }
+          }
         );
-        
       } catch (error) {
         console.error(error);
+        console.log("Failed to insert user into dataBase");
       }
     } else {
       res.send("<p>Email is already registerd please log in!</p>");
     }
   } catch (error) {
     console.error(error);
+    res.send("Failed to register user.");
+  }
+};
+export const loginUser = async (req: Request, res: Response) => {
+  const { user_email, user_password } = req.body;
+  try {
+    const result = await dataBase.query(
+      "SELECT * FROM users WHERE user_email=$1",
+      [user_email]
+    );
+    if (result.rows.length > 0) {
+      const user = result.rows[0];
+    } else {
+      res.send("Go to sign up.");
+    }
+  } catch (error) {
+    console.error(error);
+    console.log("Failed to login user.");
   }
 };
