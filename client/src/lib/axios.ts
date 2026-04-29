1521746951ee37b90995fc1e9213a0ba123f8c69
Sphesihle Mabaso <mabasosphesihle25@gmail.com>
2026-04-29 14:02:49 +0200

    fix base ur

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -1,9 +1,6 @@
 import axios from "axios";
 
-const BASE_URL =
-  import.meta.env.MODE === "development"
-    ? import.meta.env.VITE_API_URL
-    : import.meta.env.VITE_API_URL;
+const BASE_URL =import.meta.env.VITE_API_URL;
 export const serverApi = axios.create({
   baseURL: BASE_URL,
   headers: { "Content-Type": "application/json" },
