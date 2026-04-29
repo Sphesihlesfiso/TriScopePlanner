6d2663aab2b4b3ba147e322d765800e26f273120
Sphesihle Mabaso <mabasosphesihle25@gmail.com>
2026-04-29 15:05:28 +0200

    fix: set sameSite none for cross-origin cookies

--- server/src/utils/generateToken.utils.ts
+++ server/src/utils/generateToken.utils.ts
@@ -9,8 +9,8 @@ export const generateToken = async (res: Response, userId: string | number) => {
   });
   res.cookie("token", token, {
     httpOnly: true, //css attacks ,
-    secure: process.env.NODE_ENV == "production",
-    sameSite: true,
+    secure: true,
+    sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
   return token;
